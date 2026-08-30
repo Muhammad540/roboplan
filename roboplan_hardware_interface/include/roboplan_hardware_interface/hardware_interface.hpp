d522eb53b1907861b570850af466b0029379ba8e
Muhammad540 <thisisdeahmed@gmail.com>
2026-08-30 23:35:34 +0200

    make the api more explicit, for vel/tau add separate write methods

--- roboplan_hardware_interface/include/roboplan_hardware_interface/hardware_interface.hpp
+++ roboplan_hardware_interface/include/roboplan_hardware_interface/hardware_interface.hpp
@@ -12,26 +12,12 @@ namespace roboplan {
         Eigen::VectorXd v;
         Eigen::VectorXd tau;
     };
-
-    struct PositionCommand {
-        Eigen::VectorXd q;
-    };
-
-    struct VelocityCommand {
-        Eigen::VectorXd v;
-    };
-
-    struct EffortCommand {
-        Eigen::VectorXd tau;
-    };
-
-    using HardwareCommand = std::variant<PositionCommand, VelocityCommand, EffortCommand>;
-
+    
     class HardwareInterface {
         public:
             virtual ~HardwareInterface() = default;
 
             virtual tl::expected<RoboPlanRobotState, std::string> readState() = 0;
-            virtual tl::expected<void, std::string> writeCommand(const HardwareCommand& command) = 0;
+            virtual tl::expected<void, std::string> writePositionCommand(const Eigen::VectorXd& q) = 0;
     };
 } // namespace roboplan
